d4a3bc04302587a99926b523706af9aa37dfafb2
thradams <thiago.adams@gmail.com>
2017-07-12 21:47:36 -0300

    sample

--- TurboLint/Teste.c
+++ TurboLint/Teste.c
@@ -1,19 +1,20 @@
 
 
-//#include <stdbool.h>
-//#include <CRTDEFS.H>
-//#include <stdlib.h>
-
-
-typedef struct X
+typedef struct Point
 {
-    int i;
-} X;
+    int x = 1;
+    int y = 2;
+} Point;
 
+Point* Point_Create();
+
+
+Point* Point_Create(){...}
+void Point_Delete(Point*){...}
 
 
 int main()
 {
-    X x = {};
-
+    
+ 
 }
